22dc3914517e808cb744c01a3a860ee98514581b
yusukeh309 <elmo.15vc@gmail.com>
2020-03-23 23:13:02 +0900

    initial commit

---
+++ ChatAppWithFirebase/Controllers/ChatListViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ChatListViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by Uske on 2020/03/15.
+//  Copyright © 2020 Uske. All rights reserved.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    
+    private let cellId = "cellId"
+    
+    @IBOutlet weak var chatListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chatListTableView.delegate = self
+        chatListTableView.dataSource = self
+        
+        navigationController?.navigationBar.barTintColor = .rgb(red: 39, green: 49, blue: 69)
+        navigationItem.title = "トーク"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ChatListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped table view")
+        let storyboard = UIStoryboard.init(name: "ChatRoom", bundle: nil)
+        let chatRoomViewController = storyboard.instantiateViewController(withIdentifier: "ChatRoomViewController")
+        navigationController?.pushViewController(chatRoomViewController, animated: true)
+    }
+    
+}
+
+class ChatListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var partnerLabel: UILabel!
+    @IBOutlet weak var latestMessageLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImageView.layer.cornerRadius = 35
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
+
---
+++ ChatAppWithFirebase/Views/ChatRoomTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  ChatRoomTableViewCell.swift
+//  ChatAppWithFirebase
+//
+//  Created by Uske on 2020/03/18.
+//  Copyright © 2020 Uske. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomTableViewCell: UITableViewCell {
+    
+    var messageText: String? {
+        didSet {
+            guard let text = messageText else { return }
+            let witdh = estimateFrameForTextView(text: text).width + 20
+
+            messageTextViewWidthConstraint.constant = witdh
+            messageTextView.text = text
+        }
+    }
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var messageTextViewWidthConstraint: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = .clear
+        userImageView.layer.cornerRadius = 30
+        messageTextView.layer.cornerRadius = 15
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    private func estimateFrameForTextView(text: String) -> CGRect {
+        let size = CGSize(width: 200, height: 1000)
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        
+        return NSString(string: text).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)], context: nil)
+    }
+    
+}
